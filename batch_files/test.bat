5fdbaa8ae0a59de9eb4d6cec75127b678bb3e84c
matyalatte <matyalatte@gmail.com>
2023-09-15 07:18:53 +0900

    disable debug functions and error handling for release builds

--- batch_files/test.bat
+++ batch_files/test.bat
@@ -7,7 +7,7 @@ if /I "%~1"=="Debug" (
     set OPTIONS=-Dbuildtype=debug -Dlibui:buildtype=debug
 ) else (
     set BUILD_TYPE=Release
-    set OPTIONS=-Dbuildtype=release  -Dlibui:buildtype=release -Db_vscrt=mt -Db_ndebug=true -Dcpp_rtti=false -Db_lto=true
+    set OPTIONS=-Dbuildtype=release  -Dlibui:buildtype=release -Db_vscrt=mt -Db_ndebug=true -Dcpp_rtti=false -Db_lto=true -Dcpp_eh=none -Dlibui:cpp_eh=none
 )
 echo Build type: %BUILD_TYPE%
 
