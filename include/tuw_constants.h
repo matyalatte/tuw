0f043a85128c6758b2fcc45a4e32500f202bc502
matyalatte <matyalatte@gmail.com>
2024-03-02 21:34:07 +0900

    increment tuw version

--- include/tuw_constants.h
+++ include/tuw_constants.h
@@ -10,8 +10,8 @@ namespace tuw_constants {
         "       CLI tools\n";
     constexpr char TOOL_NAME[] = "Tuw";
     constexpr char AUTHOR[] = "matyalatte";
-    constexpr char VERSION[] = "0.6.2";
-    constexpr int VERSION_INT = 602;
+    constexpr char VERSION[] = "0.6.3";
+    constexpr int VERSION_INT = 603;
 
 #ifdef _WIN32
     constexpr char OS[] = "win";
