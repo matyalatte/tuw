f8702cc9cf731427bc532744f4e87be8ca3beea9
matyalatte <matyalatte@gmail.com>
2024-07-21 12:27:21 +0900

    fix workflow for windows arm

---
+++ tests/json_paths.h
@@ -0,0 +1,8 @@
+#pragma once
+// you need to copy it from examples/all_keys to the json folder
+const char *JSON_ALL_KEYS = "./json/gui_definition.json";
+
+// you need to copy the json folder to build dir.
+const char *JSON_BROKEN = "./json/broken.json";
+const char *JSON_CONFIG_ASCII = "./json/config_ascii.json";
+const char *JSON_CONFIG_UTF = "./json/config_utf.json";
