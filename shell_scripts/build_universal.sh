bf343ce0ab547cb94bf6288fbb87d8e69b77f1da
matyalatte <matyalatte@gmail.com>
2024-12-14 14:08:58 +0900

    disable universal build as default, add arm64-only build for macOS

---
+++ shell_scripts/build_universal.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Build an universal binary for macOS.
+# Tuw will be generated in ../build/${build_type}
+
+# You can specify build type as an argument like "bash build_universal.sh Debug"
+if [ "$1" = "Debug" ]; then
+    build_type="Debug"
+    preset="--native-file presets/debug.ini"
+else
+    build_type="Release"
+    preset="--native-file presets/release.ini"
+fi
+
+echo "Build type: ${build_type}"
+
+os=$(uname -s)
+options=""
+if [[ "$build_type" == "Release" ]]; then
+    options="-Db_lto=true"
+fi
+
+pushd $(dirname "$0")/..
+    meson setup build/${build_type} ${preset} -Dmacosx_universal=true ${options} || exit 1
+    cd build/${build_type}
+    meson compile -v || exit 1
+
+    # Strip symbols to reduce the binary size
+    if [ "${build_type}" = "Release" ]; then
+        if [[ "$os" == "Linux" ]]; then
+            strip --strip-all ./Tuw
+        elif [[ "$os" == "Darwin" ]]; then
+            strip ./Tuw
+        fi
+    fi
+popd
